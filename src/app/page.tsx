41c47b5015af206214f189e7f30eedea2afdf017
Sufal Thakre <sufalthakre1@gmail.com>
2026-02-03 12:07:44 +0530

    Initial Next.js project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="italic text-6xl bg-purple-600 text-pink-100">Hello world</h1>
+    </>
+  );
+}
